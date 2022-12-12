205b2988805f54985d754d2a30c5135a2f4f66f1
obipranata <obiimanuelpranata@gmail.com>
2022-12-12 09:18:33 +0700

    add templates

--- resources/views/artikel/store.blade.php
+++ resources/views/artikel/store.blade.php
@@ -1,82 +1,33 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('templates')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.ckeditor.com/ckeditor5/35.3.2/classic/ckeditor.js"></script>
-    <title>Tambah Artikel</title>
-  </head>
-  <body>
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container">
-        <a class="navbar-brand" href="#">Obito</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="{{route('produk.list')}}">Produk</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{route('artikel.index')}}">Artikel</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    
-    <div class="container mt-5">
-      <a href="{{route('artikel.index')}}" class="btn btn-success mb-3">List</a>
-      @if ($message = Session::get('success'))
-        <div class="alert alert-success" role="alert" style="width: 80%">
-          <strong>{{ $message }}</strong>
-        </div>
-      @endif
-      <div class="card" style="width: 60%">
-        <h5 class="card-header">Tambah Artikel</h5>
-        <div class="card-body">
-          <form method="post" action="{{route('artikel.create')}}" enctype="multipart/form-data">
-            @csrf
-            <div class="mb-3">
-              <label for="judul" class="form-label">Judul</label>
-              <input type="text" class="form-control" id="judul" name="judul">
-            </div>
-            <div class="mb-3">
-              <label for="isi" class="form-label">Isi</label>
-              <textarea id="editor" name="isi" rows="4" cols="50"></textarea>
-            </div>
-            <div class="mb-3">
-              <label for="foto" class="form-label">Foto</label>
-              <input type="file" class="form-control" id="foto" name="foto" accept="image/*">
-            </div>
-            <button type="submit" class="btn btn-primary">Simpan</button>
-          </form>
-        </div>
-      </div>
+@section('content')
+<div class="container mt-5">
+  <a href="{{route('artikel.index')}}" class="btn btn-success mb-3">List</a>
+  @if ($message = Session::get('success'))
+    <div class="alert alert-success" role="alert" style="width: 80%">
+      <strong>{{ $message }}</strong>
     </div>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-    <script>
-        ClassicEditor
-            .create( document.querySelector( '#editor' ) )
-            .catch( error => {
-                console.error( error );
-            } );
-    </script>
-  </body>
-</html>
+  @endif
+  <div class="card" style="width: 80%">
+    <h5 class="card-header">Tambah Artikel</h5>
+    <div class="card-body">
+      <form method="post" action="{{route('artikel.create')}}" enctype="multipart/form-data">
+        @csrf
+        <div class="mb-3">
+          <label for="judul" class="form-label">Judul</label>
+          <input type="text" class="form-control" id="judul" name="judul">
+        </div>
+        <div class="mb-3">
+          <label for="isi" class="form-label">Isi</label>
+          <textarea id="editor" name="isi" rows="4" cols="50"></textarea>
+        </div>
+        <div class="mb-3">
+          <label for="foto" class="form-label">Foto</label>
+          <input type="file" class="form-control" id="foto" name="foto" accept="image/*">
+        </div>
+        <button type="submit" class="btn btn-primary">Simpan</button>
+      </form>
+    </div>
+  </div>
+</div>
+@endsection
